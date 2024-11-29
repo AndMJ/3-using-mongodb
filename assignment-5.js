27309c45c01be23a545676a64ac80060958b4133
AndMJ <andrejoao070@gmail.com>
2024-11-29 17:34:34 +0000

    UPDATE assignment-5.js done

---
+++ assignment-5.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose")
+
+//connection
+const connection = mongoose.connect("mongodb://localhost:27017/playground")
+connection.then((result) => {
+    console.log("successfully connected to db...")
+}).catch(e => {
+    console.log("error connecting to db..", e)
+})
+
+//course schema and model mapping
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    price: {type: Number, default: 10},
+    tags: [String],
+    dateCreated: {type: Date, default: Date.now()},
+    isPublished: Boolean
+})
+const Courses = mongoose.model("courses", courseSchema)
+
+//create a course
+/*const createCourse = async (data) => {
+    const newCourse = new Courses({...data})
+    return newCourse.save();
+}
+
+const result = createCourse({
+    name: "React.js course",
+    author: "Fabio",
+    price: 34,
+    tags: ["react", "frontend"],
+    isPublished: true
+})
+
+result.then((r) => {
+    console.log("course saved!", r)
+}).catch(e => {
+    console.log("error!", e)
+})*/
+
+
+const getCoursesList = async () => {
+    return Courses.find({isPublished: true})
+        .or([
+            {
+                price: {$gte: 15 }
+            },
+            {
+                name: /.*by.*/i
+            }
+        ])
+}
+
+getCoursesList().then(result => {
+    console.log(result)
+}).catch(e => {
+    console.log("error getting: ", e)
+})
